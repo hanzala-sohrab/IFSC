1c5efeba6fa8d780387286e60780937b76257273
Hanzala Sohrab <sorb.hanzala@gmail.com>
2020-07-03 15:51:22 +0530

    Modified urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view()),
-    re_path(r'^ifsc/(?P<ifs_code>\w+)$', views.bank_details, name='bank'),
-    re_path(r'^branches/(?P<bank>[-\w]+)/(?P<city>[-\w]+)$', views.details_of_branches, name='branches'),
+    path('', views.Index.as_view(), name='index'),
+    re_path(r'^api/ifsc/(?P<ifs_code>\w+)$', views.bank_details, name='bank'),
+    re_path(r'^api/branches/(?P<bank>[-\w]+)/(?P<city>[-\w]+)$', views.details_of_branches, name='branches'),
+    re_path(r'^branches/(?P<bank>[-\w]+)/(?P<city>[-\w]+)$', views.branch, name='branch'),
+    re_path(r'^ifsc/(?P<ifs_code>\w+)$', views.bank, name='ifsc'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
