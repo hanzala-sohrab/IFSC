0c119a84269691c6156852d5793ae71c477eaaf5
Hanzala Sohrab <sorb.hanzala@gmail.com>
2020-07-03 15:52:22 +0530

    Updated views.py

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import IFSC
 from .serializers import ApiSerializer
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from .forms import BankForm
 from django.views.generic.edit import FormView
 
@@ -22,7 +22,12 @@ class Index(FormView):
         city = city.replace(' ', '-').lower()
 
         if code != '':
-            return redirect('bank', code)
+            if 'html' in self.request.POST:
+                return redirect('ifsc', code)
+            elif 'api' in self.request.POST:
+                return redirect('bank', code)
+        if 'html' in self.request.POST:
+            return redirect('branch', bank, city)
         return redirect('branches', bank, city)
 
 
@@ -58,3 +63,16 @@ def details_of_branches(request, bank, city):
     if request.method == 'GET':
         serializer = ApiSerializer(branches, many=True)
         return Response(serializer.data)
+
+
+def branch(request, bank, city):
+    bank = bank.replace('-', ' ').upper()
+    city = city.replace('-', ' ').upper()
+    state = IFSC.objects.filter(CITY=city, BANK=bank).values_list('STATE', flat=True)[0]
+    branches = IFSC.objects.filter(BANK=bank, CITY=city).all()
+    return render(request, 'api/branch.html', {'branches': branches, 'bank': bank, 'city': city, 'state': state})
+
+def bank(request, ifs_code):
+    ifs_code = ifs_code.upper()
+    ifs_code = IFSC.objects.get(IFSC=ifs_code)
+    return render(request, 'api/bank.html', {'ifsc': ifs_code})
